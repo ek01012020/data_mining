88e8b9e4fdf7a6f85f5fcf51b12fca17a994f6f3
Helen <ek01012020@gmail.com>
2020-12-12 11:39:24 +0300

    lesson_6

--- lesson_4/gb_parse/pipelines.py
+++ lesson_4/gb_parse/pipelines.py
@@ -6,14 +6,29 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+import os
+import dotenv
+from scrapy import Request
+from scrapy.pipelines.images import ImagesPipeline
 from pymongo import MongoClient
 
 class GbParsePipeline:
 
     def __init__(self):
-        self.db = MongoClient('mongodb+srv://Helen:helen911@cluster0.lb92d.mongodb.net/')['parser_hh']
+        dotenv.load_dotenv('../.env')
+        self.db = MongoClient(os.getenv('DATA_BASE'))['parser_i']
 
     def process_item(self, item, spider):
         collection = self.db[spider.name]
         collection.insert_one(item)
+        return item
+
+class GbImagePipeline(ImagesPipeline):
+
+    def get_media_requests(self, item, info):
+        image = item.get('display_url')
+        yield Request(image)
+
+    def item_completed(self, results, item, info):
+        item['display_url'] = results[0][1]
         return item
